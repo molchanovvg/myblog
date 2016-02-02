d0d2a9e5ce92e0c4dc538e932da9f2e2ae290425
molchanovvg <valeruko@gmail.com>
2016-02-02 21:57:13 +0300

    Поправлена логика, редирект на viewpost, проверка на пустые поля

--- add.php
+++ add.php
@@ -20,13 +20,10 @@ if (!isset($_SESSION) || $_SESSION['right'] != 1)
 
                             <!-- Post -->
                             <?php
-                                if (isset($_POST['submit']))
+                                $header=strip_tags(trim($_POST['header']));
+                                $record=strip_tags(trim($_POST['record']));
+                                if (isset($_POST['submit']) && (!empty($header)&& !empty($record)))
                                 {
-                                    $header=strip_tags(trim($_POST['header']));
-                                    $record=strip_tags(trim($_POST['record']));
-
-                                    if (!empty($header)&& !empty($record))
-                                    {
                                         $header=mysqli_real_escape_string($dbc,$header);
                                         $record=mysqli_real_escape_string($dbc,$record);
                                         if ($stmt_insert = mysqli_prepare($dbc, "INSERT INTO recordtable VALUES (0, NOW(), ?, ?)"))
@@ -38,31 +35,36 @@ if (!isset($_SESSION) || $_SESSION['right'] != 1)
                                             };
                                             mysqli_stmt_close($stmt_insert);
                                         };
-                                        echo '<p>Вы добавили запись.</p>';
-                                        $header="";
-                                        $record="";
+                                        $id=mysqli_insert_id($dbc);
                                         mysqli_close($dbc);
-                                    }
-                                    else
+                                        $home_url='http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).'viewpost.php?id='.$id.'';
+                                        header('Location: '.$home_url);
+                                }
+                                else
+                                {
+                                    if ((isset($_POST['submit']))&&(empty($header)))
                                     {
-                                        echo '<p>Чего-то не хватает :(.</p>';
+                                        echo 'Вы не ввели заголовок поста!';
                                     }
+                                    if ((isset($_POST['submit']))&&(empty($record)))
+                                    {
+                                        echo 'Вы не ввели пост!';
+                                    }
+                                    ?>
+                                    <p>Добавление новой записи в блог</p>
+                                    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                                        <label for="header">Заголовок:</label><br>
+                                        <input type="text" name="header" id="header" value="<?php echo $header; ?>"><br>
+                                        <label for="record">Текст:</label><br>
+                                        <textarea name="record" cols="80" rows="20" id="record"><?php echo $record?></textarea><br>
+                                        <input type="submit" value="Поделиться" name="submit">
+                                    </form>
+                                <?php
                                 }
                                 ?>
-                                <p>Добавление новой записи в блог</p>
-                                <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-                                    <label for="header">Заголовок:</label><br>
-                                    <input type="text" name="header" id="header"><br>
-                                    <label for="record">Текст:</label><br>
-                                    <textarea name="record" cols="80" rows="20" id="record"></textarea><br>
-                                    <input type="submit" value="Поделиться" name="submit">
-                                </form>
 
-
-
-
-</div>
-</div>
+                        </div>
+                    </div>
         <?php
         require_once('sidebar_t.php');
         ?>
