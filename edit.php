5381d2c00ba5b4373b85362f6f7c163d5907d1dc
molchanovvg <valeruko@gmail.com>
2016-02-03 20:03:36 +0300

    authss_t.php удалено, добавлена проверка на существование обновляемой записи, расширенные формы запросов

--- edit.php
+++ edit.php
@@ -3,46 +3,52 @@ $PageTitle='Управление';
 require_once('header_t.php');
 require_once('connectvars.php');
 require_once('connectdb_t.php');
-require_once('authss_t.php');
 
-if (!isset($_SESSION) || $_SESSION['right'] != 1)
+if (isset($_SESSION) && $_SESSION['right'] != 1)
 {
     exit('У вас нет доступа к данной странице.');
 }
 
-
 ?>
 
 <div id="wrapper">
-
             <!-- Content -->
                     <div id="content">
                         <div class="inner">
                             <?php
                                 if (isset($_POST['submit']) && (!empty($_POST['header'])&& !empty($_POST['record'])))
                                 {
-                                    $dbc = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-                                    mysqli_set_charset($dbc, "utf8");
-                                    if ($dbc->connect_error)
-                                    {
-                                        die('Error connection Mysql-server ('.$dbc->connect_error.')');
-                                    }
-                                    $head=mysqli_real_escape_string($dbc, strip_tags(trim($_POST['header'])));
-                                    $rec=mysqli_real_escape_string($dbc, strip_tags(trim($_POST['record'])));
+                                    $head = mysqli_real_escape_string($dbc, strip_tags(trim($_POST['header'])));
+                                    $rec = mysqli_real_escape_string($dbc, strip_tags(trim($_POST['record'])));
                                     // проверка на наличие такой записи add
-                                    if ($stmt_update = mysqli_prepare($dbc, "UPDATE recordtable SET head = ?, rec= ? WHERE id = ? "))
-                                    {
-                                        mysqli_stmt_bind_param($stmt_update, "ssi", $head, $rec, $_POST['id']);
-                                        if (!(mysqli_stmt_execute($stmt_update)))
-                                        {
-                                            echo 'Ошибка при изменении записи';
-                                        };
-                                        mysqli_stmt_close($stmt_update);
-                                    };
-                                    mysqli_close($dbc);
-                                    echo '<p>Статья с заголовком ' . $head . ' была изменена</p>';
-                                    echo '<a href="/viewpost.php?id=' . $_POST['id'] . '"">Просмотреть статью</a></br>';
-                                    echo '<a href="/edit.php?id=' . $_POST['id'] . '"">Продолжить редактирование</a>';
+                                    if ($stmt_select = mysqli_prepare($dbc, "SELECT id, date, head, rec FROM recordtable WHERE id=? ")) {
+                                        mysqli_stmt_bind_param($stmt_select, "i", $_POST['id']);
+                                        if (!(mysqli_stmt_execute($stmt_select))) {
+                                            echo 'Ошибка при выборке данных';
+                                        }
+                                        mysqli_stmt_store_result($stmt_select);
+                                        mysqli_stmt_bind_result($stmt_select, $id, $date, $head_, $rec_);
+                                        mysqli_stmt_fetch($stmt_select);
+
+                                        if (mysqli_stmt_num_rows($stmt_select) == 1) {
+                                            if ($stmt_update = mysqli_prepare($dbc, "UPDATE recordtable SET head = ?, rec= ? WHERE id = ? ")) {
+                                                mysqli_stmt_bind_param($stmt_update, "ssi", $head, $rec, $_POST['id']);
+                                                if (!(mysqli_stmt_execute($stmt_update))) {
+                                                    echo 'Ошибка при изменении записи';
+                                                };
+                                                mysqli_stmt_close($stmt_update);
+                                            };
+                                            mysqli_close($dbc);
+                                            echo '<p>Статья с заголовком ' . $head . ' была изменена</p>';
+                                            echo '<a href="/viewpost.php?id=' . $_POST['id'] . '"">Просмотреть статью</a></br>';
+                                            echo '<a href="/edit.php?id=' . $_POST['id'] . '"">Продолжить редактирование</a>';
+
+                                        } else {
+                                            $error_msg = 'Такой записи не существует.';
+                                        }
+                                        mysqli_stmt_close($stmt_select);
+                                        mysqli_close($dbc);
+                                    }
                                 }
                                 else
                                 {
@@ -62,9 +68,7 @@ if (!isset($_SESSION) || $_SESSION['right'] != 1)
                                     {
                                         $id=$_POST['id'];
                                     }
-                                    $dbc = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-                                    mysqli_set_charset($dbc, "utf8");
-                                    if ($stmt_select = mysqli_prepare($dbc, "select * from recordtable WHERE id=?"))
+                                    if ($stmt_select = mysqli_prepare($dbc, "select id, date, head, rec from recordtable WHERE id=?"))
                                     {
                                         mysqli_stmt_bind_param($stmt_select, "i", $id);
                                         if (!(mysqli_stmt_execute($stmt_select)))
