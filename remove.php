4baf1cc7e58a867e30bf967bbc1bbbe5d9b7b827
molchanovvg <valeruko@gmail.com>
2016-02-03 07:13:23 +0300

    Исправлено удаление записей

--- remove.php
+++ remove.php
@@ -6,14 +6,14 @@ require_once('connectdb_t.php');
 require_once('authss_t.php');
 session_start();
 
-    if ($_SESSION['right'] != 1)
+    if (isset($_SESSION)&& $_SESSION['right'] != 1)
     {
         exit('У вас нет доступа к данной странице.');
     }
 
 
 ?>
-    <div id="wrapper">
+<div id="wrapper" xmlns="http://www.w3.org/1999/html">
 
             <!-- Content -->
                     <div id="content">
@@ -21,61 +21,51 @@ session_start();
 
 
                             <?php
-                                if (isset($_GET['id'])&& isset($_GET['head']))
-                                {
-                                    $id=$_GET['id'];
-                                    $head=$_GET['head'];
-                                }
-                                else if (isset($_POST['id'])&& isset($_POST['head']))
-                                    {
-                                        $id=$_POST['id'];
-                                        $head=$_POST['head'];
-                                    }
-                                    else
-                                    {
-                                        echo '<p>Нет данных для удаления.</p>';
-                                    }
                                 if (isset($_POST['submit']))
                                 {
+                                    echo $_POST['id'];
                                     if ($_POST['confirm'] == 'Yes') {
                                         $dbc = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
                                         if ($dbc->connect_error) {
                                             die('Error connection Mysql-server (' . $dbc->connect_error . ')');
                                         }
 
-                                        $query = "DELETE FROM recordtable WHERE id = $id LIMIT 1";
-
                                         if ($stmt_delete = mysqli_prepare($dbc, "DELETE FROM recordtable WHERE id = ? LIMIT 1"))
                                         {
-                                            mysqli_stmt_bind_param($stmt_delete, "i", $id);
+                                            mysqli_stmt_bind_param($stmt_delete, "i", $_POST['id']);
                                             if (!(mysqli_stmt_execute($stmt_delete)))
                                             {
                                                 echo 'Ошибка при удалении записи';
                                             };
                                             mysqli_stmt_close($stmt_delete);
                                         };
-                                        echo '<p>Рейтинг с заголовком ' . $head . ' был удален</p>';
+                                        echo '<p>Запись блога удалена</p>';
                                         mysqli_close($dbc);
                                         $home_url='http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).'manage.php';
-                                        header('Refresh: 2; url='.$home_url);
+                                        header('Location: '.$home_url);
                                     }
                                     else
                                     {
-                                        echo '<p>Ошибка при удалении</p>';
+                                        if ($_POST['confirm']=='No')
+                                        {
+                                            $home_url='http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).'viewpost.php?id='.$_POST['id'];
+                                            header('Location: '.$home_url);
+                                        }
+
                                     }
                                 }
-                                else if (isset($id)&& isset($head))
+                                else if (isset($_GET['id']))
                                 {
-                                    echo '<p>Вы уверены что хотите удалить?</p>';
-                                    echo '<br>';
-                                    echo '<p>'.$head.'</p>';
-                                    echo '<form method="post" action="remove.php">';
-                                    echo '<input type="radio" name="confirm" value="Yes" /> Yes';
-                                    echo '<input type="radio" name="confirm" value="no" checked="checked" /> No <br>';
-                                    echo '<input type="submit" value="submit" name="submit"/>';
-                                    echo '<input type="hidden" name="id" value="'.$id.'"/>';
-                                    echo '<input type="hidden" name="head" value="'.$head.'"/>';
-                                    echo '</form>';
+                                    ?>
+                                    <p>Вы уверены что хотите удалить?</p>
+                                    <br>
+                                    <form method="post" action="remove.php">
+                                        <input type="radio" name="confirm" value="Yes" /> Yes
+                                        <input type="radio" name="confirm" value="No" checked="checked" /> No <br>
+                                        <input type="submit" value="submit" name="submit"/>
+                                        <input type="hidden" name="id" value="<?php echo $_GET['id']; ?>"/>
+                                    </form>
+                                    <?php
                                 }
 
                             ?>
