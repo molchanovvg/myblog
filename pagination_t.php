ddb7894293103aac796c7d1065ca5f67f1010ad5
molchanovvg <valeruko@gmail.com>
2016-02-09 20:37:52 +0300

    В отдельный файл навигация по страницам

---
+++ pagination_t.php
@@ -0,0 +1,13 @@
+<?php
+for ($i=1; $i<=$num_pages; $i++)
+{
+    if ($cur_page==$i)
+    {
+        echo '<a href="/index.php?page='.$i.'" class="active">'.$i.'</a>';
+    }
+    else
+    {
+        echo '<a href="/index.php?page='.$i.'">'.$i.'</a>';
+    }
+
+}
