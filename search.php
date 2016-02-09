8fa778c6255b5f1597a222191b28a110ab736b3f
molchanovvg <valeruko@gmail.com>
2016-02-09 07:05:33 +0300

    Поиск по нескольким словам

--- search.php
+++ search.php
@@ -4,6 +4,51 @@ require_once('header_t.php');
 require_once('connectvars.php');
 require_once('connectdb_t.php');
 session_start();
+
+function refValues($arr)
+{       if (strnatcmp(phpversion(),'5.3') >= 0)
+        {
+            //Если версия PHP >=5.3 (в младших версиях все проще)
+            $refs = array();
+            foreach($arr as $key => $value)
+            {
+                $refs[$key] = &$arr[$key]; //Массиву $refs присваиваются ссылки на значения массива $arr
+            }
+            return $refs; //Массиву $arr присваиваются значения массива $refs
+         }
+        return $arr; //Возвращается массив $arr
+}//function refValues($arr)
+
+    $type='';
+    $search=trim(strip_tags($_POST['search']));
+    $clean_search=str_replace(',',' ',$search);
+    $search_word=explode(' ', $clean_search);
+    $final_search_word=array();
+    if (count($search_word)>0)
+    {
+        foreach ($search_word as $word)
+        {
+            if (!empty($word))
+            {
+                $final_search_word[]='%'.$word.'%';
+                $where_list[]="rec LIKE ?";
+                $type.='s';
+            }
+
+        }
+        
+
+    }
+  /*  if (count($final_search_word)>0)
+    {
+        foreach($final_search_word as $word)
+        {
+            $where_list[]="rec LIKE ?";
+            $type.='s';
+        }
+    }*/
+    $where_clause = implode(' OR ', $where_list);
+    $query="SELECT id, date, head, rec FROM recordtable WHERE ".$where_clause;
 ?>
 <div id="wrapper">
     <!-- Content -->
@@ -13,10 +58,11 @@ session_start();
             <?php
             if (isset($_POST['submit']))
             {
-                $search='%'.trim(strip_tags($_POST['search'])).'%';
-                if ($stmt_select = mysqli_prepare($dbc, "SELECT id, date, head, rec FROM recordtable WHERE rec LIKE ? "))
+               // $search='%'.trim(strip_tags($_POST['search'])).'%';
+                if ($stmt_select = mysqli_prepare($dbc, $query)) // "SELECT id, date, head, rec FROM recordtable WHERE rec LIKE ? "
                 {
-                    mysqli_stmt_bind_param($stmt_select, "s", $search);
+                    call_user_func_array('mysqli_stmt_bind_param', array_merge(array($stmt_select, $type), refValues($final_search_word)));
+                    //mysqli_stmt_bind_param($stmt_select, "s", $search); // work
                     if (!(mysqli_stmt_execute($stmt_select)))
                     {
                         exit ('Ошибка при выборке записей: ' . mysqli_stmt_error($stmt_select));
