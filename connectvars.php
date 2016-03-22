7455245261468b8ed6ec6ae9b70ea14885d8037e
Pavel Volyntsev <pvolyntsev@users.noreply.github.com>
2016-03-23 00:22:22 +0600

    connectvars.php may be reconfigured using connectvars.local.php

--- connectvars.php
+++ connectvars.php
@@ -1,5 +1,18 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '123');
-define('DB_NAME','myblog-db');
+if (is_file('./connectvars.local.php'))
+    include_once('./connectvars.local.php');
+
+if (!defined('ENV'))
+    define('ENV', 'dev');
+
+if (!defined('DB_HOST'))
+    define('DB_HOST', 'localhost');
+
+if (!defined('DB_USER'))
+    define('DB_USER', 'root');
+
+if (!defined('DB_PASSWORD'))
+    define('DB_PASSWORD', '123');
+
+if(!defined('DB_NAME'))
+    define('DB_NAME','myblog-db');
