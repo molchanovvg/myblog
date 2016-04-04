aa45748b0697194a96b205502a7f986d87f38527
molchanovvg <valeruko@gmail.com>
2016-04-04 20:11:39 +0300

    Изменена инициализация переменных для соединения с бд, добавлен режим разделения режима разработки и продакшна

---
+++ connectvars.prod.php
@@ -0,0 +1,15 @@
+<?php
+if (!defined('ENV'))
+    define('ENV', 'prod');
+
+if (!defined('DB_HOST'))
+    define('DB_HOST', '127.0.0.1');
+
+if (!defined('DB_USER'))
+    define('DB_USER', 'molchano_wp251');
+
+if (!defined('DB_PASSWORD'))
+    define('DB_PASSWORD', '67U]97JSP]');
+
+if (!defined('DB_NAME'))
+    define('DB_NAME', 'molchano_myblog');
