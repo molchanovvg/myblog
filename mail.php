c2e4fdbc134959a09406c00f0eff6a0bcc325b16
molchanovvg <valeruko@gmail.com>
2016-04-06 21:52:02 +0300

    Добавлена функция отправки ошибки по почту на сервере production

---
+++ mail.php
@@ -0,0 +1,25 @@
+<?php
+function SendMail($message_body){
+    require_once 'lib/swift_required.php';
+    // Create the mail transport configuration
+    $transport = Swift_SmtpTransport::newInstance("smtp.yandex.ru", 465);
+    $transport->setUsername("molchanov256@yandex.ru");
+    $transport->setPassword("356893005542996");
+    $transport->setEncryption('ssl');
+
+// Create the message
+    $message = Swift_Message::newInstance('-f %s');
+    $message->setTo(array(
+        "valeruko@gmail.com" => "valeruko"
+    ));
+    $message->setSubject('Error site myblog');
+    $message->setBody($message_body);
+    $message->setFrom("molchanov256@yandex.ru", "Myblog");
+
+// Send the email
+    $mailer = Swift_Mailer::newInstance($transport);
+    $mailer->send($message, $failedRecipients);
+}
+
+
+
