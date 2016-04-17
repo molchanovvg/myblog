1da2e3c31a12b04b706716d80b53b3ccb947db8a
molchanovvg <valeruko@gmail.com>
2016-04-17 14:13:09 +0300

    Мелкие изменения

--- mail.php
+++ mail.php
@@ -1,21 +1,21 @@
 <?php
 function SendMail($message_body){
 
-
+    global $config;
     // Create the mail transport configuration
-    $transport = Swift_SmtpTransport::newInstance(Server_SMTP, 465);
-    $transport->setUsername(Server_Name);
-    $transport->setPassword(Server_Pass);
+    $transport = Swift_SmtpTransport::newInstance($config->getParam('Server_SMTP'), 465);
+    $transport->setUsername($config->getParam('Server_Name'));
+    $transport->setPassword($config->getParam('Server_Pass'));
     $transport->setEncryption('ssl');
 
 // Create the message
     $message = Swift_Message::newInstance('-f %s');
     $message->setTo(array(
-        Server_To => Server_To_Name
+        $config->getParam('Server_To') => $config->getParam('Server_To_Name')
     ));
     $message->setSubject('Error site myblog');
     $message->setBody($message_body);
-    $message->setFrom(Server_From, "Myblog");
+    $message->setFrom($config->getParam('Server_From'), "Myblog");
 
 // Send the email
     $mailer = Swift_Mailer::newInstance($transport);
