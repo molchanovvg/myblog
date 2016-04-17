247b5891ff904651ed3f8336344ea68a128976b7
molchanovvg <valeruko@gmail.com>
2016-04-17 14:12:21 +0300

    Изменен способ загрузки параметров

--- core.php
+++ core.php
@@ -1,8 +1,26 @@
 <?php
+
+
+// Подключаем конфигурацию
+$params=include('params.php');
+if (is_file('params.dev.php'))
+{
+    $params = array_merge($params, include('params.dev.php'));
+}
+if (is_file('params.prod.php'))
+{
+    $params = array_merge($params, include('params.prod.php'));
+}
+
+require_once( "Configuration.php" );
+$config = new Configuration($params);
+
+// Свой обработчик ошибок
 require_once('error_handler.php');
 
-require_once('connectvars.php');
+// коннект к БД
 require_once('connectdb_t.php');
 
+// библиотека для отправки почты
 if (is_file('./vendor/autoload.php'))
     include_once('./vendor/autoload.php');
