ee37944e9942bb57f789b375638822842eeb8c7e
molchanovvg <valeruko@gmail.com>
2016-04-17 14:11:07 +0300

    Добавлен файл для хранения параметров

---
+++ params.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    'ENV' => 'dev',
+    'DB_HOST' => 'localhost',
+    'DB_USER' => 'root',
+    'DB_PASSWORD' => '123',
+    'DB_NAME' => 'myblog-db',
+    'Server_SMTP' => 'smtp_server',
+    'Server_Name' => 'name',
+    'Server_Pass' => 'pass',
+    'Server_To' => 'email',
+    'Server_To_Name' => 'name_to',
+    'Server_From' => '"email_from"',
+);
