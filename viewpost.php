d50c40700a0130bb11ea4dba415df7a8c93a8fcf
molchanovvg <valeruko@gmail.com>
2016-04-17 14:13:50 +0300

    Изменения в логике

--- viewpost.php
+++ viewpost.php
@@ -1,30 +1,27 @@
 <?php
 require_once('core.php');
+
 $head='';
-if (!isset($_GET['id'])&&(!isset($_POST['submit']))){
+$recordId =isset($_GET['id']) ? $_GET['id'] : (isset($_POST['id']) ? $_POST['id'] : null); // record id from URL or post data
+
+
+if (empty($recordId))
+{
     include('404.php');
-}
-if (empty($_GET['id']))
-{
-    $get_id=$_POST['id'];
-}
-else
-{
-    $get_id=$_GET['id'];
-}
-#if (isset($_GET['id']))
-{
-    if ($stmt_select = mysqli_prepare($dbc, "SELECT id, date, head, rec FROM recordtable WHERE id=?")) {
-        mysqli_stmt_bind_param($stmt_select, "i", $get_id);
-        if (!(mysqli_stmt_execute($stmt_select))) {
-
+} else {
+    if ($stmt_select = mysqli_prepare($dbc, "SELECT id, date, head, rec FROM recordtable WHERE id=?"))
+    {
+        mysqli_stmt_bind_param($stmt_select, "i", $recordId);
+        if (!(mysqli_stmt_execute($stmt_select)))
+        {
+            trigger_error(mysqli_stmt_error($stmt_select), E_USER_WARNING);
             exit ('Ошибка при выборке записей: ' . mysqli_stmt_error($stmt_select));
-
         };
         mysqli_stmt_store_result($stmt_select);
         mysqli_stmt_bind_result($stmt_select, $id, $date, $head, $rec);
         mysqli_stmt_fetch($stmt_select);
-        if (mysqli_stmt_num_rows($stmt_select) == 0){
+        if (mysqli_stmt_num_rows($stmt_select) == 0)
+        {
             mysqli_stmt_close($stmt_select);
             include('404.php');
         }
@@ -33,12 +30,11 @@ else
 };
 
 
-
 $PageTitle=$head.' просмотр записи в php блоге';
 require_once('header_t.php');
 
 session_start();
-$errors = array();
+$errors = array(); // form processing error messages
 ?>
 <div id="wrapper">
     <!-- Content -->
@@ -47,44 +43,37 @@ $errors = array();
             <?php
             if ((isset($_POST['submit'])))
             {
-                if ($_SESSION['pass']==sha1($_POST['verify']))
+                if ($_SESSION['pass']!=sha1($_POST['verify'])) // check captcha
                 {
-                    if (empty($_POST['commit']))
+                    $errors['verify'] = 'Captcha введена не верно!';
+                }
+                $commit=trim(strip_tags($_POST['commit']));
+                if (strlen($commit)<20)
+                {
+                    $errors['commit'] = 'Текст комментария должен быть не менее 20 символов';
+                }
+                if (empty($errors))
+                {
+                    if ($stmt_insert = mysqli_prepare($dbc, "INSERT INTO commenttable(commid, date, postid, userid, comment) VALUES (0, NOW(), ?, ?, ?)"))
                     {
-                        $errors['commit']='Введите комментарий!';
-                    }
-                    else
-                    {
-                        $commit=trim(strip_tags($_POST['commit']));
-                        if ($stmt_insert = mysqli_prepare($dbc, "INSERT INTO commenttable(commid, date, postid, userid, comment) VALUES (0, NOW(), ?, ?, ?)"))
+                        mysqli_stmt_bind_param($stmt_insert, "sss", $recordId , $_SESSION['user_id'], $commit);
+                        if (!(mysqli_stmt_execute($stmt_insert)))
                         {
-
-                            mysqli_stmt_bind_param($stmt_insert, "sss", $_POST['id'] , $_SESSION['user_id'], $commit);
-                            if (!(mysqli_stmt_execute($stmt_insert)))
-                            {
-                                // exit ('Ошибка при добавлении записи: '.mysqli_stmt_error($stmt_insert));
-                                $errors['_form']='Ошибка при добавлении комментария!';
-                            };
-                            mysqli_stmt_close($stmt_insert);
-                        };
-                        mysqli_close($dbc);
-                        if (empty($errors))
-                        {
-                            echo '<p>Вы добавили комментарий.</p>';
-                            $_SESSION['commit']='';
-                            $home_url='http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).'viewpost.php?id='.$_POST['id'].'';
-                            header('Location: '.$home_url);
+                            trigger_error(mysqli_stmt_error($stmt_insert), E_USER_WARNING);
+                            $errors['_form'] = 'Ошибка при добавлении комментария';
                         }
+                        mysqli_stmt_close($stmt_insert);
                     }
-
+                    mysqli_close($dbc);
+                    echo '<p>Вы добавили комментарий.</p>';
+                    $home_url='http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).'viewpost.php?id='.$recordId.'';
+                    header('Location: '.$home_url);
                 }
-                else
-                {
+            }
+            if ($recordId)
+            {
 
-                    $errors['verify']='Captcha введена не верно!';
-                }
-
-            }?>
+            ?>
                 <article class="box post post-excerpt">
                         <header>
                             <h2><?php echo $head ?></h2>
@@ -103,67 +92,62 @@ $errors = array();
                 // Вывод комментариев
                 if ($stmt_select = mysqli_prepare($dbc, "SELECT t1.username, t2.date, t2.comment FROM mybloguser as t1, commenttable as t2 WHERE t2.postid=?  AND t2.userid = t1.userid order by t2.date desc"))
                 {
-                    mysqli_stmt_bind_param($stmt_select, "i", $get_id);
+                    mysqli_stmt_bind_param($stmt_select, "i", $recordId);
                     if (!(mysqli_stmt_execute($stmt_select)))
                     {
-                        //exit ('Ошибка при выборке записей: '.mysqli_stmt_error($stmt_select));
-                        $errors['_form']='Запись не найдена!';
-                    };
-                    mysqli_stmt_bind_result($stmt_select, $name, $date, $comm);
-                    while (mysqli_stmt_fetch($stmt_select))
+                        trigger_error(mysqli_stmt_error($stmt_select), E_USER_WARNING);
+                        ?><div class="bg-danger">Комментарии не найдены</div><?php
+                    } else
                     {
-                        ?>
-                        <article class="box post post-excerpt">
-                            <header>
-                                <h1><?php echo $name.'  '.$date ?></h1>
-                            </header>
-                            <p>
-                                <?php echo $comm?>
-                            </p>
-                        </article>
-                        <?php
-                    };
-
-                    mysqli_stmt_close($stmt_select);
-
+                        mysqli_stmt_bind_result($stmt_select, $name, $date, $comm);
+                        while (mysqli_stmt_fetch($stmt_select))
+                        {
+                            ?>
+                            <article class="box post post-excerpt">
+                                <header>
+                                    <h1><?php echo $name.'  '.$date ?></h1>
+                                </header>
+                                <p>
+                                    <?php echo $comm?>
+                                </p>
+                            </article>
+                            <?php
+                        };
+                        mysqli_stmt_close($stmt_select);
+                    }
                 };
                 // Генерация блока с вводом комментариев
                 if (isset($_SESSION['user_id']))
                 {
-
-                    if (!empty($errors['_form']))
-                    {?>
-                        <div class="error"><?php echo $errors['_form']?></div>
-                    <?php
-                    }
-                    ?>
+                    if (!empty($errors['_form'])) { ?>
+                        <div class="bg-danger"><?php echo $errors['_form'] ?></div>
+                    <?php } ?>
                     <!-- New commit -->
                     <p>Добавить комментарий</p>
                     <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
                         <label for="commit">Комментарий:</label><br>
-                        <textarea name="commit" cols="40" rows="10" id="commit"><?php if (!isset($_POST['commit'])){ echo '';}else{ echo $_POST['commit'];} ?></textarea><br>
-                        <?php if (!empty($errors['commit']))
-                        {?>
-                            <div class="error"><?php echo $errors['commit']?></div>
+                        <textarea name="commit" cols="40" rows="10" id="commit"><?php echo isset($_POST['commit']) ? htmlentities($_POST['commit']) : '' ?></textarea><br>
+                        <?php if (!empty($errors['commit'])) { ?>
+                            <div class="bg-danger"><?php echo $errors['commit'] ?></div>
                         <?php } ?>
                         <label for="verify">Защита от роботов:</label><br>
                         <img src="captcha.php"><br>
                         <input type="text" name="verify" placeholder="Введите буквы с картинки"><br>
-                        <?php if (!empty($errors['verify']))
-                        {?>
-                            <div class="error"><?php echo $errors['verify']?></div>
-                            <?php } ?>
+                        <?php if (!empty($errors['verify'])) { ?>
+                            <div class="bg-danger"><?php echo $errors['verify'] ?></div>
+                        <?php } ?>
                         <input type="submit" value="Прокомментировать" name="submit">
-                        <input type="hidden" value="<?php echo $get_id?>" name="id">
+                        <input type="hidden" value="<?php echo $recordId ?>" name="id">
                     </form>
                     <?php
                 }
                 else
                 {
-                    echo 'Вы не авторизованы, авторизуйтесь, чтобы добавлять комментарии.';
+                    echo 'Авторизуйтесь, чтобы добавлять комментарии!';
                 }
                 mysqli_close($dbc);
-                ?>
+            };
+            ?>
 
         </div>
     </div>
